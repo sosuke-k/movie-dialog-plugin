110d0379a08fd29e477eb9a7825627dd57da9eb1
sosuke-k <snoopies.drum@gmail.com>
2016-05-18 09:13:06 +0900

    Initialize Kibana Plugin (generator-kibana-plugin v1.0.1)

---
+++ index.js
@@ -0,0 +1,30 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      app: {
+        title: 'Movie Dialog Plugin',
+        description: 'An awesome Kibana plugin',
+        main: 'plugins/movie_dialog_plugin/app'
+      },
+      hacks: [
+        'plugins/movie_dialog_plugin/hack'
+      ]
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+
+  });
+};
